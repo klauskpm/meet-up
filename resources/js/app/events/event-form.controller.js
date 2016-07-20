b7e29198864a8804d81f9424680278b397411b56
Klaus Kazlauskas <klausmachado@gmail.com>
2016-07-20 20:37:12 -0300

    Events form html
    All inputs are displayed. It needs to be reordered and polished.

--- resources/js/app/events/event-form.controller.js
+++ resources/js/app/events/event-form.controller.js
@@ -2,6 +2,9 @@
 	var injectParams = ['$scope', '$mdDialog', 'currentEvent'];
 	var EventFormController = function ($scope, $mdDialog, currentEvent) {
 		$scope.currentEvent = currentEvent;
+
+		if (!$scope.currentEvent.guestList || !$scope.currentEvent.guestList.length)
+			$scope.currentEvent.guestList = [];
 	};
 
 	EventFormController.$inject = injectParams;
